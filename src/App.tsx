448e0e3a54b2cd598782f3f84341fe3206e9ec97
Pooya Deperson <poamrd@gmail.com>
2025-09-02 17:45:37 +0200

    update-html-structure-output
    update-html-structure-output
    1. camera feed and camera drop containers
    and other containers.

--- src/App.tsx
+++ src/App.tsx
@@ -101,28 +101,44 @@ function App() {
 
   return (
     <div className="App">
-      <CameraPermissions onStreamReady={handleStreamReady} />
-      <div {...getRootProps({ className: 'dropzone' })}>
-        <p>Drag & drop RPM avatar GLB file here</p>
-      </div>
-      <input
-        className="url"
-        type="text"
-        placeholder="Paste RPM avatar URL"
-        onChange={(e) =>
-          setUrl(`${e.target.value}?morphTargets=ARKit&textureAtlas=1024`)
-        }
-      />
-      <video className="camera-feed" id="video" autoPlay playsInline></video>
-      <Canvas style={{ height: 600 }} camera={{ fov: 25 }} shadows>
-        <ambientLight intensity={0.5} />
-        <pointLight position={[10, 10, 10]} color={new Color(1, 1, 0)} intensity={0.5} castShadow />
-        <pointLight position={[-10, 0, 10]} color={new Color(1, 0, 0)} intensity={0.5} castShadow />
-        <pointLight position={[0, 0, 10]} intensity={0.5} castShadow />
-        <Avatar url={url} />
-      </Canvas>
-      <img className="logo" src="./logo.png" />
+  {/* Camera Section */}
+  <div className="camera-container">
+    <CameraPermissions onStreamReady={handleStreamReady} />
+    <video className="camera-feed" id="video" autoPlay playsInline></video>
+  </div>
+
+  {/* Avatar Input Section */}
+  <div className="avatar-input-container">
+    <div {...getRootProps({ className: 'dropzone' })}>
+      <p>Drag & drop RPM avatar GLB file here</p>
     </div>
+    <input
+      className="url-input"
+      type="text"
+      placeholder="Paste RPM avatar URL"
+      onChange={(e) =>
+        setUrl(`${e.target.value}?morphTargets=ARKit&textureAtlas=1024`)
+      }
+    />
+  </div>
+
+  {/* Avatar Canvas Section */}
+  <div className="avatar-canvas-container">
+    <Canvas style={{ height: 800 }} camera={{ fov: 25 }} shadows>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} color={new Color(1, 1, 0)} intensity={0.5} castShadow />
+      <pointLight position={[-10, 0, 10]} color={new Color(1, 0, 0)} intensity={0.5} castShadow />
+      <pointLight position={[0, 0, 10]} intensity={0.5} castShadow />
+      <Avatar url={url} />
+    </Canvas>
+  </div>
+
+  {/* Logo Section */}
+  <div className="logo-container">
+    <img className="logo" src="./logo.png" alt="Logo" />
+  </div>
+</div>
+
   );
 }
 
