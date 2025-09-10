1a970bf256708631523e093980918280ebdd80af
Pooya Deperson <poamrd@gmail.com>
2025-09-10 22:48:12 +0200

    camera={{ fov: 27,           position: [0, 0, 4]

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
       <Canvas
         className="avatar-container bottom-0 pos-abs z-1"
         camera={{ fov: 27,
-          position: [0, 0, 5] 
+          position: [0, 0, 4] 
         }} // ~50mm equivalent and moved closer
         shadows
       >
