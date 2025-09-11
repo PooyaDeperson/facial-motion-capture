6a4f64983ddb8f92d9dcb36f69f6284bead1e9c4
Pooya Deperson <poamrd@gmail.com>
2025-09-11 16:23:00 +0200

    bringing back faulty icons

--- src/camera-permission.tsx
+++ src/camera-permission.tsx
@@ -1,18 +1,18 @@
 import { useEffect, useState } from "react";
 import CustomDropdown, { Option } from "./components/CustomDropdown";
 
-// const CameraIcon = (
-//   <svg className="w-4 h-4 text-gray-600" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
-//     <path strokeLinecap="round" strokeLinejoin="round" d="M3 7h2l2-3h10l2 3h2v13H3V7z" />
-//     <circle cx="12" cy="13" r="3" stroke="currentColor" strokeWidth="2" />
-//   </svg>
-// );
+const CameraIcon = (
+  <svg className="w-4 h-4 text-gray-600" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M3 7h2l2-3h10l2 3h2v13H3V7z" />
+    <circle cx="12" cy="13" r="3" stroke="currentColor" strokeWidth="2" />
+  </svg>
+);
 
-// const VideoIcon = (
-//   <svg className="w-4 h-4 text-gray-600" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
-//     <path strokeLinecap="round" strokeLinejoin="round" d="M15 10l4.553-2.276a1 1 0 011.447.894v8.764a1 1 0 01-1.447.894L15 14M4 6h11a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V8a2 2 0 012-2z" />
-//   </svg>
-// );
+const VideoIcon = (
+  <svg className="w-4 h-4 text-gray-600" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M15 10l4.553-2.276a1 1 0 011.447.894v8.764a1 1 0 01-1.447.894L15 14M4 6h11a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V8a2 2 0 012-2z" />
+  </svg>
+);
 
 function PermissionPopup({ title, subtitle, buttonText, onClick, showButton }: any) {
   return (
