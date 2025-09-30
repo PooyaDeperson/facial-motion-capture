823ebebfcd9bddb50e6bb6c1e586080177b6eb98
Pooya Deperson <poamrd@gmail.com>
2025-09-30 21:32:47 +0200

    swthicing between avatars fixed

--- src/components/AvatarSwitcher.tsx
+++ src/components/AvatarSwitcher.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
-import './AvatarSwitcher.css';
+import React, { useEffect } from "react";
+import "./AvatarSwitcher.css";
 
 interface AvatarSwitcherProps {
   onAvatarChange: (newUrl: string) => void;
+  activeUrl: string | null;
 }
 
-const AvatarSwitcher: React.FC<AvatarSwitcherProps> = ({ onAvatarChange }) => {
+const AvatarSwitcher: React.FC<AvatarSwitcherProps> = ({ onAvatarChange, activeUrl }) => {
   const avatars = [
-    { name: 'Avatar 1', url: 'avatar/avatar1.glb' },
-    { name: 'Avatar 2', url: 'avatar/avatar2.glb' }
+    { name: "Avatar 1", url: "avatar/avatar1.glb" },
+    { name: "Avatar 2", url: "avatar/avatar2.glb" },
   ];
+
+  useEffect(() => {
+    if (!activeUrl) {
+      onAvatarChange(avatars[0].url);
+    }
+  }, []);
 
   return (
     <div className="avatar-switcher">
       {avatars.map((avatar) => (
-        <button key={avatar.name} onClick={() => onAvatarChange(avatar.url)}>
+        <button
+          key={avatar.name}
+          onClick={() => onAvatarChange(avatar.url)}
+          className={`avatar-btn ${activeUrl === avatar.url ? "active" : ""}`}
+        >
           {avatar.name}
         </button>
       ))}
